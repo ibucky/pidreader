e31f06ed108b70435f82facdda3c82ad056e07a8
Bucky Harmos <ibuckyharmos@gmail.com>
2020-03-27 23:02:06 -0600

    First commit

---
+++ ConsoleUI/PID.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class PID
+    {
+        public string frame { get; set; }
+        public string batteryVoltage { get; set; }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\VCS\ALJ820052EngineData1.txt";
+
+            List<PID> timeFrame = new List<PID>();
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            foreach (string line in lines)
+            {
+                string[] column = line.Split('\t');
+
+                PID newPid = new PID();
+                newPid.frame = column[0];
+                newPid.batteryVoltage = column[1];
+
+                timeFrame.Add(newPid);
+            }
+
+            foreach (var timeSegment in timeFrame)
+            {
+                Console.WriteLine($"{timeSegment.frame} {timeSegment.batteryVoltage}");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
